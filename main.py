8a674cace01be20205dae420ec55388e460787c3
ladyada <limor@ladyada.net>
2017-10-16 19:06:44 -0400

    working demo! tested w/two sensors

--- main.py
+++ main.py
@@ -6,18 +6,21 @@ import ds18x20
 ow =  OneWire.OneWire(board.D2)
 ds18 = ds18x20.DS18X20(ow)
 
-print("Resetting!",ow.reset())
+def pretty_address(addr):
+    return ''.join('{:02x}'.format(x) for x in addr)
+
+print("Resetting, found anything?",ow.reset())
 
 print("Found the following 1-Wire devices: ", end="")
 for dev in ow.scan():
-    address = ''.join('{:02x}'.format(x) for x in dev)
-    print(address)
-
-print("Found the following DS18x20 devices: ", end="")
-for dev in ds18.scan():
-    address = ''.join('{:02x}'.format(x) for x in dev)
-    print(address)
+    print(pretty_address(dev))
+print("")
 
 while True:
+    print("Found the following DS18x20 devices")
     for dev in ds18.scan():
-	print(ds18.read_temp(dev))
+	ds18.convert_temp()
+	time.sleep(1)
+	print(pretty_address(dev), ":", ds18.read_temp(dev))
+    print("--------------")
+    time.sleep(1)
