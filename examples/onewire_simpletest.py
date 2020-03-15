80a2d656ba931f4d6242228f119c8854e66abac5
dherrada <dylan.herrada@gmail.com>
2020-03-15 14:54:00 -0400

    Ran black, updated to pylint 2.x

--- examples/onewire_simpletest.py
+++ examples/onewire_simpletest.py
@@ -7,14 +7,14 @@ ow_bus = OneWireBus(board.D2)
 
 # Reset and check for presence pulse.
 # This is basically - "is there anything out there?"
-print("Resetting bus...", end='')
+print("Resetting bus...", end="")
 if ow_bus.reset():
     print("OK.")
 else:
     raise RuntimeError("Nothing found on bus.")
 
 # Run a scan to get all of the device ROM values
-print("Scanning for devices...", end='')
+print("Scanning for devices...", end="")
 devices = ow_bus.scan()
 print("OK.")
 print("Found {} device(s).".format(len(devices)))
@@ -22,9 +22,9 @@ print("Found {} device(s).".format(len(devices)))
 # For each device found, print out some info
 for i, d in enumerate(devices):
     print("Device {:>3}".format(i))
-    print("\tSerial Number = ", end='')
+    print("\tSerial Number = ", end="")
     for byte in d.serial_number:
-        print("0x{:02x} ".format(byte), end='')
+        print("0x{:02x} ".format(byte), end="")
     print("\n\tFamily = 0x{:02x}".format(d.family_code))
 
 # Usage beyond this is device specific. See a CircuitPython library for a 1-Wire
