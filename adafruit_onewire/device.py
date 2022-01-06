b1e3ac2157339a691cc4c2cac6dae7c442436c00
Alec Delaney <tekktrik@gmail.com>
2022-01-06 12:50:48 -0500

    Add type hints

--- adafruit_onewire/device.py
+++ adafruit_onewire/device.py
@@ -14,24 +14,31 @@ Provides access to a single device on the 1-Wire bus.
 __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_OneWire.git"
 
+try:
+    from typing import Optional, Type
+    from types import TracebackType
+    from adafruit_onewire.bus import OneWireBus, OneWireAddress
+except ImportError:
+    pass
+
 _MATCH_ROM = b"\x55"
 
 
 class OneWireDevice:
     """A class to represent a single device on the 1-Wire bus."""
 
-    def __init__(self, bus, address):
+    def __init__(self, bus: OneWireBus, address: OneWireAddress):
         self._bus = bus
         self._address = address
 
-    def __enter__(self):
+    def __enter__(self) -> 'OneWireDevice':
         self._select_rom()
         return self
 
     def __exit__(self, exception_type: Optional[Type[type]], exception_value: Optional[BaseException], traceback: Optional[TracebackType]) -> bool:
         return False
 
-    def readinto(self, buf, *, start=0, end=None):
+    def readinto(self, buf: bytearray, *, start: int = 0, end: Optional[int] = None) -> None:
         """
         Read into ``buf`` from the device. The number of bytes read will be the
         length of ``buf``.
@@ -49,7 +56,7 @@ class OneWireDevice:
             if self._bus.crc8(buf):
                 raise RuntimeError("CRC error.")
 
-    def write(self, buf, *, start=0, end=None):
+    def write(self, buf: bytearray, *, start: int = 0, end: Optional[int] = None) -> None:
         """
         Write the bytes from ``buf`` to the device.
 
@@ -63,7 +70,7 @@ class OneWireDevice:
         """
         return self._bus.write(buf, start=start, end=end)
 
-    def _select_rom(self):
+    def _select_rom(self) -> None:
         self._bus.reset()
         self.write(_MATCH_ROM)
         self.write(self._address.rom)
